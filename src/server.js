407667f184e0c5e1e2729e8f27e9834afb0ac003
Brian Jordan <206973+bcjordan@users.noreply.github.com>
2023-10-13 23:43:54 -0400

    Initial commit from PartyKit

---
+++ src/server.js
@@ -0,0 +1,47 @@
+/* eslint-env browser */
+
+// @ts-check
+// Optional JS type checking, powered by TypeScript.
+/** @typedef {import("partykit/server").Party} Party */
+/** @typedef {import("partykit/server").Server} Server */
+/** @typedef {import("partykit/server").Connection} Connection */
+/** @typedef {import("partykit/server").ConnectionContext} ConnectionContext */
+
+/**
+ * @implements {Server}
+ */
+class PartyServer {
+  constructor(party) {
+    /** @type {Party} */
+    this.party = party;
+  }
+
+  /**
+   * @param {Connection} conn - The connection object.
+   * @param {ConnectionContext} ctx - The context object.
+   */
+  onConnect(conn, ctx) {
+    // A websocket just connected!
+    console.log(
+      `Connected:
+  id: ${conn.id}
+  room: ${this.party.id}
+  url: ${new URL(ctx.request.url).pathname}`
+    );
+
+    // Send a message to the connection
+    conn.send("hello from server");
+  }
+
+  /**
+   * @param {string} message
+   * @param {Connection} sender
+   */
+  onMessage(message, sender) {
+    console.log(`connection ${sender.id} sent message: ${message}`);
+    // Broadcast the received message to all other connections in the room except the sender
+    this.party.broadcast(`${sender.id}: ${message}`, [sender.id]);
+  }
+}
+
+export default PartyServer;
